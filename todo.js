1bcbcb80dc5501172d38dbced8a1667e6513c927
Kristine1985 <39009714+Kristine1985@users.noreply.github.com>
2019-11-01 10:22:39 +0100

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,27 @@
+
+var myTaskArray = []
+
+var date = new Date();
+  $(document).ready(function () {
+    $("#addBtn").click(function () {
+        var newItem = $('#inputUser').val();
+        myTaskArray.push(newItem);
+        if (myTaskArray.length === 11) {
+            alert("Your Schedule for today is now full!");
+        } else {
+            $("#list").append("<li><input type = 'checkbox' id = 'removeTask'>" + "  " + newItem + "</li>");
+        }
+        $('#inputUser').val('');
+    });
+
+   
+    $("#removeBtn").click(function () {
+        $('#list').children().filter(function () {  
+            return this.firstChild.checked;
+        }).remove();
+    });
+
+});
+
+
+
