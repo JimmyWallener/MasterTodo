e4d986d358fb6b2fe2c7dbaf794da7536442e576
Jimmy Wallener <jimmy.wallener@protonmail.com>
2019-11-05 00:15:00 +0100

    Update todo.js
    La dit en if statement som stoppar tomma listor och kräver minimum 4 karaktärer för att posta. Returnerar ingen alert, utan inget sker bara (return)

--- todo.js
+++ todo.js
@@ -1,10 +1,7 @@
 
 var myTaskArray = []
 
-var date = new Date(); // vad är poängen med den här raden? det står ingen datum någonstans på sidan!
-// Syftet är nog att sätta datummärkning på todo, dock behöver den formateras till YY/MM/DD men visar YYYY/MM/DD/TZ
-// ok jag förstår!
-
+var date = new Date();
 
 $(document).ready(function () {
 
@@ -13,6 +10,8 @@ $(document).ready(function () {
     myTaskArray.push(newItem);
     if (myTaskArray.length > 10) {
       alert("Your Schedule for today is now full!");
+    } if (newItem == '' || newItem.length < 4) {
+      return;
     } else {
       $("#list").append("<li><input type = 'checkbox' id = 'removeTask'>" + "  " + newItem + "</li>");
     }
