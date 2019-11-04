178917b2d169271934782aab09339b6b0ea60b80
Jimmy Wallener <38855083+jimwdev@users.noreply.github.com>
2019-11-04 10:26:14 +0100

    Update todo.js
    Besvarade fråga :P

--- todo.js
+++ todo.js
@@ -1,25 +1,26 @@
 
 var myTaskArray = []
 
-var date = new Date(); // vad är poängen med den här raden? det står ingen datum någonstans på sidan! 
-  $(document).ready(function () {
-    $("#addBtn").click(function () {
-        var newItem = $('#inputUser').val();
-        myTaskArray.push(newItem);
-        if (myTaskArray.length === 11) {
-            alert("Your Schedule for today is now full!");
-        } else {
-            $("#list").append("<li><input type = 'checkbox' id = 'removeTask'>" + "  " + newItem + "</li>");
-        }
-        $('#inputUser').val('');
-    });
+var date = new Date(); // vad är poängen med den här raden? det står ingen datum någonstans på sidan!
+// Syftet är nog att sätta datummärkning på todo, dock behöver den formateras till YY/MM/DD men visar YYYY/MM/DD/TZ
+$(document).ready(function () {
+  $("#addBtn").click(function () {
+    var newItem = $('#inputUser').val();
+    myTaskArray.push(newItem);
+    if (myTaskArray.length === 11) {
+      alert("Your Schedule for today is now full!");
+    } else {
+      $("#list").append("<li><input type = 'checkbox' id = 'removeTask'>" + "  " + newItem + "</li>");
+    }
+    $('#inputUser').val('');
+  });
 
-   
-    $("#removeBtn").click(function () {
-        $('#list').children().filter(function () {  
-            return this.firstChild.checked;
-        }).remove();
-    });
+
+  $("#removeBtn").click(function () {
+    $('#list').children().filter(function () {
+      return this.firstChild.checked;
+    }).remove();
+  });
 
 });
 
