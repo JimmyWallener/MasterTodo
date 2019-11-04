d4218627cf16aa122c8ceef1f1eab5e86f3a6141
Kristine1985 <39009714+Kristine1985@users.noreply.github.com>
2019-11-04 15:10:53 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,11 +4,14 @@ var myTaskArray = []
 var date = new Date(); // vad är poängen med den här raden? det står ingen datum någonstans på sidan!
 // Syftet är nog att sätta datummärkning på todo, dock behöver den formateras till YY/MM/DD men visar YYYY/MM/DD/TZ
 // ok jag förstår!
+
+
 $(document).ready(function () {
+  
   $("#addBtn").click(function () {
     var newItem = $('#inputUser').val();
     myTaskArray.push(newItem);
-    if (myTaskArray.length === 11) {
+    if (myTaskArray.length > 10) {
       alert("Your Schedule for today is now full!");
     } else {
       $("#list").append("<li><input type = 'checkbox' id = 'removeTask'>" + "  " + newItem + "</li>");
