70a85cf2dbda9e41722c28ed121de2fa5fdedf3a
Jimmy Wallener <38855083+jimwdev@users.noreply.github.com>
2019-11-06 13:37:19 +0100

    Update todo.js
    Fick ta bort myTaskArray.lenght för tillfället då remove function inte fungerar, då den checkar första värdet.

--- todo.js
+++ todo.js
@@ -15,7 +15,7 @@ $(function () {
     } if (newItem == '' || newItem.length < 4) {
       return;
     } else {
-      $("#list").append("<li>" + "[" + myTaskArray.length + "]" + "<input type = 'checkbox' id = 'removeTask'>" + "  " + newItem + "</li>");
+      $("#list").append("<li><input type = 'checkbox' id = 'removeTask'>" + "  " + newItem + "</li>");
       // lade till nummer framför texten.
       //när man klickar på checkbox i listan ska det alternativet strykas över. Jag har addat här också :) 
       //jag testar gitbash here och det funkar.. hehehe im so happy -- Älskar det 
@@ -38,7 +38,10 @@ $(function () {
 
 
   //
-
+  $(function () {
+    (".hoverText[title]").tooltip();
+  });
+  // Lade till en tooltip för att uppmärksamma besökaren om att klicka för att byta färg.
   $("#removeBtn").click(function () {
     $('#list').children().filter(function () {
       return this.firstChild.checked;
@@ -47,10 +50,7 @@ $(function () {
   // Gjorde containern klickbar istället för random bakgrundsfärg. Fick deaktivera css för image gradient då den lägger sig över backgroundsfärgen.
   // Ändrade igen, då färgen ändrades när man använder textfältet. Nu är det h1 texten som är klickbar, dock är klickable area stort nog.
   //ohhh nice :)
-  (function () {
-    (".hoverText[title]").tooltip();
-  });
-  // Lade till en tooltip för att uppmärksamma besökaren om att klicka för att byta färg.
+
   $("h1").click(function () {
     $(".container").each(function () {
       let colorRed = Math.floor((Math.random() * 256));
